574a9ef9d1905bec5c112a38f5c6d577555e18bc
Hugo Branco <hugobrancowb@gmail.com>
2022-12-12 21:07:21 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Hugo Branco on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
